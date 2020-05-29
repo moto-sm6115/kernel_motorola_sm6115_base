7174f8c3e9a6a86aba111a1cef56152c83a70f1b
zhouwl <zhouwl@lenovo.com>
2020-05-29 15:31:12 +0800

    aw882xx: set default impedance value
    If audio calibration doesn't run, no aw_cali.bin file,
    it will set 0 as speaker impedance, it's not safe.
    Set 7 as default value.
    
    Change-Id: I2369a3abc3f9ca7417ede88164cdf60a0319e3ac
    Signed-off-by: zhouwl <zhouwl@mt.com>
    Reviewed-on: https://gerrit.mot.com/1587157
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- asoc/codecs/awinic/aw882xx.c
+++ asoc/codecs/awinic/aw882xx.c
@@ -479,9 +479,8 @@ static void aw882xx_start(struct aw882xx *aw882xx)
 
 	ret = aw882xx_get_cali_re_form_nv(&cali_re);
 	if (ret < 0) {
-		cali_re = ERRO_CALI_VALUE;
-		pr_err("%s: use default vaule %d",
-			__func__ , ERRO_CALI_VALUE);
+		cali_re = (DEFAULT_CALI_VALUE << 12);
+		pr_err("%s: use default vaule %d", __func__ , cali_re);
 	}
 	ret = aw882xx_set_cali_re(aw882xx, cali_re);
 	if (ret < 0)
